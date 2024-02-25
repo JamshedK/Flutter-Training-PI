d48ec186e945e9f70963d1718fc55cd5a621cd00
Blake Lee <137326318+blake-lee-38@users.noreply.github.com>
2024-02-25 13:02:32 -0600

    Panel changes and correcting menu icon

--- lib/faq_page.dart
+++ lib/faq_page.dart
@@ -14,91 +14,92 @@ class _FAQPageState extends State<FAQPage> {
     super.initState();
   }
 
-  final List<bool> _isOpen = [false, false, false, false];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        //TODO: How to get the default back button?
+        //TODO: Pull out into a reusable appBar [Use "___" extends AppBar]
+        leading: Navigator.canPop(context)
+            ? IconButton(
+                icon: const Icon(Icons.navigate_before_sharp,
+                    color: Colors.white),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              )
+            : null,
+        iconTheme: const IconThemeData(color: Colors.white),
         backgroundColor: primaryColor,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            IconButton(
-              icon: const Icon(Icons.arrow_back, color: Colors.white),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-            const Text('FAQ\'s',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                )),
-            IconButton(
-              icon: const Icon(Icons.menu),
+        centerTitle: true,
+        title: const Text("FAQ's",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+              height: 1.5,
+            )),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: IconButton(
+              icon: Image.asset('assets/PI_menu_icon.png',
+                  width: 20.0, height: 16.0),
               color: Colors.white,
               onPressed: () {},
             ),
-          ],
-        ),
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
             const SizedBox(height: 10),
-            _faqBox('The average time for ER at this facility is...',
-                '1 Hour, 33 Minutes', 0),
+            _faqBox(
+              'The average time for ER at this facility is...',
+              '1 Hour, 33 Minutes',
+            ),
             const SizedBox(height: 10),
             _faqBox(
-                'Is my Information Safe?',
-                'Yes! Patient Inform has hand selected secure tools that help our app remain '
-                    'HIPAA compliant and keep your personal health information both safe and secure!',
-                1),
+              'Is my Information Safe?',
+              'Yes! Patient Inform has hand selected secure tools that help our app remain '
+                  'HIPAA compliant and keep your personal health information both safe and secure!',
+            ),
             const SizedBox(height: 10),
             _faqBox('Does this have any hidden charges?',
-                'No! Patient Inform is completely free to use!', 2),
+                'No! Patient Inform is completely free to use!'),
             const SizedBox(height: 10),
             _faqBox('How far back does my history go?',
-                'We have your 1 year old history.', 3),
+                'We have your 1 year old history.'),
           ],
         ),
       ),
     );
   }
 
-  Widget _faqBox(titleText, bodyText, index) => Container(
+  Widget _faqBox(titleText, bodyText) => Container(
         decoration: BoxDecoration(
           border: Border.all(color: const Color(0x22000000), width: 1.5),
           borderRadius: BorderRadius.circular(10),
         ),
         padding: const EdgeInsets.all(5),
-        child: ExpansionPanelList(
-          elevation: 0,
-          expandIconColor: primaryColor,
-          expandedHeaderPadding: const EdgeInsets.all(10),
+        child: ExpansionTile(
+          //Empty border parameter removes default divider lines
+          shape: const Border(),
+          collapsedIconColor: primaryColor,
+          iconColor: primaryColor,
+          title: Text(
+            titleText,
+            style: const TextStyle(fontSize: 14),
+          ),
           children: [
-            ExpansionPanel(
-              headerBuilder: (context, isOpen) => ListTile(
-                title: Text(
-                  titleText,
-                  style: const TextStyle(fontSize: 14),
-                ),
+            ListTile(
+              title: Text(
+                bodyText,
+                style:
+                    const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
               ),
-              body: ListTile(
-                title: Text(
-                  bodyText,
-                  style: const TextStyle(
-                      fontSize: 14, fontWeight: FontWeight.bold),
-                ),
-              ),
-              isExpanded: _isOpen[index],
             ),
           ],
-          expansionCallback: (int i, bool isOpen) =>
-              setState(() => _isOpen[index] = isOpen),
         ),
       );
 }
