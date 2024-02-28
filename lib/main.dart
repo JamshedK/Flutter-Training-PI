32222187241af00ae4907b276ceff658833b13b4
Blake Lee <137326318+blake-lee-38@users.noreply.github.com>
2024-02-27 22:21:55 -0600

    Completed FAQ Page - Needs Review (#8)
    * Completed FAQ Page
    
    * Panel changes and correcting menu icon
    
    * Created ThemedAppBar component

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
+// import 'package:tutorial/faq_page.dart';
 import 'package:tutorial/splash/splash_screen.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
