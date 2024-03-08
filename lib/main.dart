093a75a13228761e10506a0a096d5b0766db1933
Ameer <officialameerghazal@outlook.com>
2024-03-07 19:10:57 -0600

    find

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:tutorial/homepage.dart';
 import 'package:tutorial/profile_screen.dart';
 // import 'package:tutorial/faq_page.dart';
 import 'package:tutorial/splash/splash_screen.dart';
@@ -35,7 +36,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(fontFamily: 'Poppins'),
       debugShowCheckedModeBanner: false,
-      home: const ProfilePage(),
+      home: const NotificationsPage(),
     );
   }
 }
