8b6e79433ffc1df826e5701becfab84026ffc01e
Ameer <officialameerghazal@outlook.com>
2024-03-07 19:12:02 -0600

    ded

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(fontFamily: 'Poppins'),
       debugShowCheckedModeBanner: false,
-      home: const NotificationsPage(),
+      home: const SplashScreen(),
     );
   }
 }
